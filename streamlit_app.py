3e799d92556b7468328f44ccc5287717edc22114
SUSHITH K S <sushithks@gmail.com>
2025-04-18 16:25:51 -0400

    Update streamlit_app.py
    REST API  Integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -52,5 +53,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered , {name_on_order}!', icon="✅")
 
-
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     
