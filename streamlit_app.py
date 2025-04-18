4ce70db5f88825092c4f1b1bc8c83ff28735069c
sushith <sushithks@gmail.com>
2025-04-18 17:04:31 -0400

    SEARCH_ON column changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 name_on_order = st.text_input("Name on Smoothie:")
@@ -35,7 +36,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        st.subheader(fruit_choosen + 'Nutrition Information')
+        st.subheader(fruit_choosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
