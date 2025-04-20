0bc2d216e7ca21d1660a233fc469905adfa89f66
sushith <sushithks@gmail.com>
2025-04-20 17:30:29 -0400

    SEARCH_ON fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,6 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered , {name_on_order}!', icon="✅")
+        st.success(f'Your Smoothie order is placed , {name_on_order}!', icon="✅")
 
 
